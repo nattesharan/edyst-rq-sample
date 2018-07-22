c5cbd8146efbba25c37760d672b7cd03deefe338
Sharan <sharan@esthenos.com>
2018-07-22 18:25:10 +0530

    we can view the job status

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from redis import Redis
 from rq import Queue
+from rq.job import Job
 app = Flask(__name__)
 engine = create_engine('sqlite:///test.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -12,7 +13,8 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 app.secret_key = 'TESTAPP'
-q = Queue(connection=Redis())
+conn = Redis()
+q = Queue(connection=conn)
 
 def init_db():
     from models import *
@@ -25,7 +27,7 @@ def submit_url():
     if request.method ==  'POST':
         from tasks import save_summary
         if request.form['url']:
-            task = q.enqueue(save_summary, args=(request.form['url'],), result_ttl=5000)
+            task = q.enqueue(save_summary, args=(request.form['url'],), result_ttl=86400)
             print task.get_id()
             flash('Success')
         else:
@@ -36,5 +38,16 @@ def get_summary():
     from models import summary
     summary_list = summary.query.all()
     return render_template('home.html',summary_list=summary_list)
+
+@app.route('/job/<job_key>', methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+    print job.is_finished
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Failed", 202
+
 if __name__  == '__main__':
     app.run(port=8080,debug=True)
