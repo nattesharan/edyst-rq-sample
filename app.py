e67a77c1ff49140f9d216c6666e3f4fb0b171714
Sharan <sharan@esthenos.com>
2018-07-22 18:25:10 +0530

    changes

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask,request,render_template,flash
 from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
 from rq import Queue
+from rq.job import Job
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'TESTAPP'
 db = SQLAlchemy(app)
-q = Queue(connection=Redis())
+conn = Redis()
+q = Queue(connection=conn)
 @app.route('/index',methods=['GET','POST'])
 def submit_url():
     if request.method == 'GET':
@@ -15,7 +17,7 @@ def submit_url():
     if request.method ==  'POST':
         from tasks import save_summary
         if request.form['url']:
-            task = q.enqueue(save_summary, args=(request.form['url'],), result_ttl=5000)
+            task = q.enqueue(save_summary, args=(request.form['url'],), result_ttl=86400)
             print task.get_id()
             flash('Success')
         else:
@@ -26,5 +28,16 @@ def get_summary():
     from models import summary
     summary_list = summary.query.all()
     return render_template('home.html',summary_list=summary_list)
+
+@app.route('/job/<job_key>', methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+    print job.is_finished
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Failed", 202
+
 if __name__  == '__main__':
     app.run(port=8080,debug=True)
--- tasks.py
+++ tasks.py
@@ -3,8 +3,12 @@ import requests
 from models import summary 
 
 def save_summary(url):
-    resp = requests.get(url)
-    word_count = len(resp.text.split())
-    data = summary(url,word_count)
-    db.session.add(data)
-    db.session.commit()
+    try:
+        resp = requests.get(url)
+        word_count = len(resp.text.split())
+        data = summary(url,word_count)
+        db.session.add(data)
+        db.session.commit()
+        return {'result':data.id,'success':True}
+    except:
+        return {'result':'Some error occured', 'success': False}
